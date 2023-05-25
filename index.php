7d0e22ebcbae181401542558a8beae26a3bb4580
shintanurlaela <118157445+shintanurlaela@users.noreply.github.com>
2023-05-25 10:20:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ADIN</title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: #DB7093;
+      }
+    table {
+      border: solid 1px #FFF8DC;
+      border-collapse: collapse;
+      border-spacing: 0;
+      width: 70%;
+      margin: 10px auto 10px auto;
+    }
+    table thead th {
+        background-color: #FFF5EE;
+        border: solid 1px #FFC0CB;
+        color: #FFC0CB;
+        padding: 10px;
+        text-align: left;
+        text-shadow: 1px 1px 1px #fff;
+        text-decoration: none;
+    }
+    table tbody td {
+        border: solid 1px #D8BFD8;
+        color: #D8BFD8;
+        padding: 10px;
+        text-shadow: 1px 1px 1px #fff;
+    }
+    a {
+          background-color: #FFE4E1;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+    }
+    </style>
+  </head>
+  <body>
+    <center><h1>Data Siswa XI TKJ 2</h1><center>
+    <center><a href="tambah_produk.php">+ &nbsp; Tambah</a><center>
+    <br/>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Dekripsi</th>
+          <th>Kelas</th>
+          <th>Absen</th>
+          <th>Foto</th>
+          <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+      $query = "SELECT * FROM produk ORDER BY id ASC";
+      $result = mysqli_query($koneksi, $query);
+      //mengecek apakah ada error ketika menjalankan query
+      if(!$result){
+        die ("Query Error: ".mysqli_errno($koneksi).
+           " - ".mysqli_error($koneksi));
+      }
+
+      //buat perulangan untuk element tabel dari data mahasiswa
+      $no = 1; //variabel untuk membuat nomor urut
+      // hasil query akan disimpan dalam variabel $data dalam bentuk array
+      // kemudian dicetak dengan perulangan while
+      while($row = mysqli_fetch_assoc($result))
+      {
+      ?>
+       <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $row['nama_produk']; ?></td>
+          <td><?php echo substr($row['deskripsi'], 0, 20); ?>...</td>
+          <td><?php echo number_format($row['harga_beli'],0,',','.'); ?></td>
+          <td><?php echo $row['harga_jual']; ?></td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;"></td>
+          <td>
+              <a href="edit_produk.php?id=<?php echo $row['id']; ?>">Edit</a> |
+              <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+          </td>
+      </tr>
+         
+      <?php
+        $no++; //untuk nomor urut terus bertambah 1
+      }
+      ?>
+    </tbody>
+    </table>
+  </body>
+</html>
---
+++ tambah_produk.php
@@ -0,0 +1,78 @@
+<?php
+  include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ADIN</title>
+    <style type="text/css">
+      * {
+        font-family: "Trebuchet MS";
+      }
+      h1 {
+        text-transform: uppercase;
+        color: salmon;
+      }
+    button {
+          background-color: salmon;
+          color: #fff;
+          padding: 10px;
+          text-decoration: none;
+          font-size: 12px;
+          border: 0px;
+          margin-top: 20px;
+    }
+    label {
+      margin-top: 10px;
+      float: left;
+      text-align: left;
+      width: 100%;
+    }
+    input {
+      padding: 6px;
+      width: 100%;
+      box-sizing: border-box;
+      background: #f8f8f8;
+      border: 2px solid #ccc;
+      outline-color: salmon;
+    }
+    div {
+      width: 100%;
+      height: auto;
+    }
+    .base {
+      width: 400px;
+      height: auto;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      background: #ededed;
+    }
+    </style>
+  </head>
+  <body>
+      <center>
+        <h1>Tambah Data</h1>
+      <center>
+      <form method="POST" action="proses_tambah.php" enctype="multipart/form-data" >
+      <section class="base">
+        <div>
+          <label>Nama</label>
+          <input type="text" name="nama" autofocus="" required="" />
+        </div>
+        <div>
+          <label>Kelas</label>
+         <input type="text" name="deskripsi" />
+        </div>
+        <div>
+          <label>Foto</label>
+         <input type="file" name="foto" required="" />
+        </div>
+        <div>
+         <button type="submit">Simpan</button>
+        </div>
+        </section>
+      </form>
+  </body>
+</html>
